019011be34be557ae3dac31847129b9b5770c5fe
near <nguyenlamsang2004@gmail.com>
2023-10-14 22:41:20 +0700

    fix conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+
 import SideMenu from "./components/sideMenu/page";
 import Dashboard from "./components/dashboard/page";
 import HeaderComponent from "./components/header/page";
 import "./globals.css";
-import AccountSetting from "./pages/account/page";
+
 export default function Home() {
   return (
     <div className="container">
